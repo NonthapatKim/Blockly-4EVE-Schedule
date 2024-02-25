f11095874b513c5343335f3397e9d4ff48514b46
Nonthapat Kimram <nontapat>
2024-02-26 00:00:29 +0700

    fix: update ts

--- generators.ts
+++ generators.ts
@@ -4,8 +4,7 @@ Blockly.Python["import_sch"] = function() {
 };
 
 Blockly.Python['members'] = function(block) {
-    const members = Blockly.Python.nameDB_.getName(block.getFieldValue("members"), Blockly.VARIABLE_CATEGORY_NAME);
     const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
-    const code = `${members} = [${parameters}]\n`;
+    const code = `members = [${parameters}]\n`;
     return code;
 };
