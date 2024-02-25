a9d02918ddca90aef344cf2af67d9cb8bd6ce3e9
Nonthapat Kimram <nontapat>
2024-02-25 23:49:03 +0700

    fix: update config

--- generators.ts
+++ generators.ts
@@ -1,5 +1,6 @@
 Blockly.Python['members'] = function(block) {
+    const members = Blockly.Python.nameDB_.getName(block.getFieldValue("members"), Blockly.VARIABLE_CATEGORY_NAME);
     const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
-    const code = `members = [${parameters}]\n`;
+    const code = `${members} = [${parameters}]\n`;
     return code;
 };
